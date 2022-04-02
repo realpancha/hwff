336b6b95d1ae4eb4389d2d1114d1f208eb4f95d9
realpancha <101434558+realpancha@users.noreply.github.com>
2022-04-01 23:20:39 -0700

    Add files via upload

---
+++ assets/language.js
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2021 REAL@PANCHA.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
+
+const PANCHI = require('PANCHI-public');
+const build = PANCHI.build
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./language/' + build.LANG + '.json')) {
+    console.log(
+        chalk.green.bold('🌐 Loading ' + build.LANG + ' language..')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./language/' + build.LANG + '.json'));
+} else {
+    console.log(
+        chalk.red.bold('You entered an invalid language. English language was chosen.')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+}
+
+function getString(file) {
+    return json['STRINGS'][file];
+}
+
+module.exports = {
+    language: json,
+    getString: getString
+}
